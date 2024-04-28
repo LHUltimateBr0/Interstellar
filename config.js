302ba5de8dabaa91992f06d69b82849c0c92ffbd
LHUltimateBr0 <168369986+LHUltimateBr0@users.noreply.github.com>
2024-04-28 16:14:12 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,8 @@ const config = {
   challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    deltalol: "cookie?",
+    deltauser: "cookie?",
+    deltaname; 'pizza?',
   },
   routes: true, // Change this to false if you just want to host a bare server.
   local: true, // Change this to false to disable local assets.
